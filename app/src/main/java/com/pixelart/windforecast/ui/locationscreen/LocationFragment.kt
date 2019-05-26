bcd6de63031a3dd4a5593e6054bb8753d2574287
Justice <popystone@gmail.cm>
2019-05-26 20:03:36 +0100

    added implementation to display favourite locations

--- app/src/main/java/com/pixelart/windforecast/ui/locationscreen/LocationFragment.kt
+++ app/src/main/java/com/pixelart/windforecast/ui/locationscreen/LocationFragment.kt
@@ -2,20 +2,24 @@ package com.pixelart.windforecast.ui.locationscreen
 
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pixelart.windforecast.AppController
-
 import com.pixelart.windforecast.R
+import com.pixelart.windforecast.adapter.LocationsAdapter
 import com.pixelart.windforecast.di.fragment.FragmentModule
+import kotlinx.android.synthetic.main.fragment_location.view.*
 import javax.inject.Inject
 
-class LocationFragment : Fragment() {
+class LocationFragment : Fragment(), LocationsAdapter.OnItemClickedListener {
     private lateinit var rootView: View
+    private lateinit var rvAdapter: LocationsAdapter
 
     @Inject lateinit var viewModel: LocationViewModel
 
@@ -26,26 +30,39 @@ class LocationFragment : Fragment() {
             .applicationComponent
             .newFragmentComponent(FragmentModule(this))
         fragmentComponent.injectLocationScreen(this)
+
+        rvAdapter = LocationsAdapter(this)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_location, container, false)
-
-        viewModel.addLocation("Hatfield,UK")
-
-        viewModel.getLocations().observe(this, Observer {
-            for (location in it){
-                Log.d("Locations", "${location.name} ${it.size}")
-            }
-        })
-
-        viewModel.showErrorMessage().observe(this, Observer {
-            Log.d("ERRM", it)
-        })
-
         return rootView
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        rootView.rvLocations.apply {
+            layoutManager = LinearLayoutManager(this@LocationFragment.context)
+            addItemDecoration(DividerItemDecoration(this@LocationFragment.context, LinearLayoutManager.VERTICAL))
+            adapter = rvAdapter
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        viewModel.getLocations().observe(this, Observer {locations ->
+            rvAdapter.submitList(locations)
+        })
+
+        viewModel.showErrorMessage().observe(this, Observer {
+            Toast.makeText(activity, it, Toast.LENGTH_LONG).show()
+        })
+    }
+
+    override fun onItemClicked(position: Int) {
+
+    }
 }
